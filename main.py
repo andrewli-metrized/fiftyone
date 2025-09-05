ed98988800f8fd49fe71ad7b4b255a64d1447a8a
Andrew Li <andrewl@metrized.com>
2025-09-05 09:59:39 -0700

    wait

--- main.py
+++ main.py
@@ -3,5 +3,4 @@ import fiftyone.zoo as foz
 
 dataset = foz.load_zoo_dataset("quickstart")
 session = fo.launch_app(dataset)
-print("FiftyOne app running at:", session.url)
-input("Press Enter to quit")
+session.wait()
